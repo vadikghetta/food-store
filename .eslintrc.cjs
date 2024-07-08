c36f0d32a97b1c92c47fc18f93bbf8d1bd680ad4
vadikghetta <helloiamvadik@mail.ru>
2024-07-08 22:55:55 +0300

    eslint ignore files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:react-hooks/recommended",
     ],
-    ignorePatterns: ["build"],
+    ignorePatterns: ["build", ".eslintrc.cjs", "jest.config.js"],
     parser: "@typescript-eslint/parser",
     plugins: ["react-refresh"],
     rules: {
